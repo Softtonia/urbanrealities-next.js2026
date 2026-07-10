9a3f8799af3010afc0c1052473df4174db24e004
vkwebdev <vijay.kumar@softtonia.com>
2026-07-10 11:42:13 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,58 +1,25 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true, 
-
-//   swcMinify: true, 
-//   compress: true,  
-
-//   productionBrowserSourceMaps: false, 
-
-//   experimental: {
-//     turbo: true,
-//     optimizePackageImports: ["lodash", "date-fns"], 
-//   },
-
-//   webpack(config, { dev, isServer }) {
-//     if (dev) {
-//       config.devtool = false; 
-//     }
-
-
-//     return config;
-//   },
-
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "api.urbanrealities.com",
-//       },
-//     ],
-//     domains: ["images.unsplash.com"],
-//   },
-// };
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone",
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "api.urbanrealities.com",
+        hostname: "api.holiplaces.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
-    domains: ["images.unsplash.com"],
   },
 
-  webpack(config, { dev, isServer }) {
+  webpack(config, { dev }) {
     if (dev) {
-      config.devtool = false; // Development me debugging tool off karna (optional)
+      config.devtool = false;
     }
+
     return config;
   },
 };
