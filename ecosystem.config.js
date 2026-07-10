6ff85a5294a0a7967cfd8c213438fb3c109ee9b4
vkwebdev <vijay.kumar@softtonia.com>
2026-07-10 11:39:32 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,18 @@
 module.exports = {
-    apps: [
-        {
-            name: "urbanrealities",
-            script: "npm",
-            args: "start",
-            env: {
-                NODE_ENV: "production",
-                X_CLIENT_ID: "LD5TNUNOKREYBI2",
-                X_CLIENT_SECRET: "8UJIIPRSUJK4NNI",
-                LARAVEL_API_BASE_URL: "https://api.holiplaces.com",
-                NEXT_PUBLIC_API_URL: "https://api.holiplaces.com",
-                NEXT_PUBLIC_BUSINESS_DOMAIN : "https://business.holiplaces.com",
-                X_APP_TYPE: "website",
-                NEXTJS_INTERNAL_KEY: "XS7N2XMQNAXKMM0XYYWF1EGGZEWHHWXAYQPGX8RV1YNXHHLR1D"
-            }
-        }
-    ]
+  apps: [
+    {
+      name: 'holiplaces',
+      cwd: '/var/www/holiplaces.com/current',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 3000 -H 127.0.0.1',
+      exec_mode: 'fork',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '700M',
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
 };
