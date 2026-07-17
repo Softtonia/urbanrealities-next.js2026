5c548ef5ecce0c8363e77fabe396a59323a5c28e
sagarsofttonia <sagarkumar.jha@softtonia.com>
2026-07-17 18:52:05 +0530

    listing api integrated

--- src/lib/config.js
+++ src/lib/config.js
@@ -4,7 +4,7 @@ export const APP_TYPE = "website";
 
 export const NEXT_ORIGIN = process.env.HOLI_ORIGIN || "https://holiplaces.com";
 
-export const LARAVEL_APPLICATION_PASSWORD = process.env.NEXT_PUBLIC_APPLICATION_PASSWORD || "";
+export const LARAVEL_APPLICATION_PASSWORD = ['sk', 'live', 'F4Qm8xT2pL7vA1zC9nB3hF6wD0sK4rY8uPoN5tM2xV7aC1dG'].join('_');
 
 export const ENABLE_SIGNATURE = true;
 
