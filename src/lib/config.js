6eebd52f941aacbdac16cf0a58d4e7d60d1a0f56
DiyaManocha <145923872+DiyaManocha@users.noreply.github.com>
2026-08-20 11:50:37 +0530

    fix config.js issue: use env variables instead of hardcoded secrets and un-ignore config.js

---
+++ src/lib/config.js
@@ -0,0 +1,9 @@
+export const LARAVEL_API_BASE_URL = process.env.NEXT_PUBLIC_LARAVEL_API_BASE_URL || "https://api.holiplaces.com";
+export const APP_TYPE = process.env.NEXT_PUBLIC_APP_TYPE || "website";
+export const NEXT_ORIGIN = process.env.NEXT_PUBLIC_NEXT_ORIGIN || "https://holiplaces.com";
+export const LARAVEL_APPLICATION_PASSWORD = process.env.LARAVEL_APPLICATION_PASSWORD || "";
+export const ENABLE_SIGNATURE = process.env.NEXT_PUBLIC_ENABLE_SIGNATURE === 'true' || true;
+export const NEXT_PUBLIC_SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://holiplaces.com";
+export const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL || "https://api.holiplaces.com";
+export const NEXT_PUBLIC_BUSINESS_DOMAIN = process.env.NEXT_PUBLIC_BUSINESS_DOMAIN || "https://business.holiplaces.com";
+
